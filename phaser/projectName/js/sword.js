ea094a90d51899f2f54c19a779e64f1cfdd73f60
RjewUcsc <rjew@ucsc.edu>
2019-05-01 16:58:07 -0700

    Create sword.js

---
+++ phaser/projectName/js/sword.js
@@ -0,0 +1,57 @@
+function Sword (game, key, x, y, sword_size)
+{
+   // Set the sprite
+   Phaser.Sprite.call(this, game, 128, 48, key, 0);
+
+   //Physics properties
+   game.physics.enable(this);
+   this.anchor.x = 0;
+   this.anchor.y = 0.5;
+   this.x = x;
+   this.y = y;
+   this.alpha = 1;
+   this.angle.set(90);
+   this.delay = 15;
+   this.end_lag = 15;
+   this.swinging = false;
+   this.hit_wall = false;
+   this.hit_angle = 0;
+
+}
+
+Sword.prototype = Object.create(Phaser.Sprite.prototype);
+Sword.prototype.constructor = Sword;
+
+Sword.prototype.update = function () {
+   this.x = player.x;
+   this.y = player.y;
+
+   if(this.delay > 0) {
+      this.delay--;
+   } else {
+      this.swinging = true;
+   }
+
+   if(this.swinging == true) {
+      this.angle -= 9;
+      this.wall_check();
+      if(angle <= 0) {
+         this.swinging = false;
+      }
+   }
+
+   if(this.delay <= 0 && this.swinging == false)
+   {
+      if(this.end_lag > 0)
+      {
+         this.end_lag--;
+      } else {
+         this.kill();
+      }
+   }
+
+}
+
+Sword.prototype.wall_check = function () {
+
+}
