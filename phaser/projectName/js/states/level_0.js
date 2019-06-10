8dc09eedd41bbcacf79a69c8ce1d2672ad79fdd8
Benjamin Roisen <broisen@ucsc.edu>
2019-06-09 19:51:43 -0700

    updated tutorial

--- phaser/projectName/js/states/level_0.js
+++ phaser/projectName/js/states/level_0.js
@@ -11,8 +11,7 @@ var loadLevel_0 = function(game, player, platforms, enemies, orePots, exit, ores
 
 	//change the background to reflect the new level
 	background.loadTexture('background_0');
-	background.scale.setTo(0.5);
-
+	
 	//resize the world to match the new level dimensions
 	game.world._definedSize = false;	//this lets us make the world smaller than it is currently
 	game.world.resize(1000,600);
@@ -24,143 +23,31 @@ var loadLevel_0 = function(game, player, platforms, enemies, orePots, exit, ores
 	player.body.moves = true;	//re-enable physics movement
 
 	//spawn some tutorial arrows
-	player.arrowLeft = new Arrow(game, 'keyL', 100, game.world.height - 200);
+	player.arrowLeft = new Arrow(game, 'keyL', 575, 216);
 	game.add.existing(player.arrowLeft);
-	player.arrowRight = new Arrow(game, 'keyR', 180, game.world.height - 200);
+	player.arrowRight = new Arrow(game, 'keyR', 655, 216);
 	game.add.existing(player.arrowRight);
-
-	// //reset the player's position
-	// // player.body.x = 140;
-	// // player.body.y = (game.world.height - 160);
- //   player.arrowLeft = new Arrow(game, 'keyL', 100, game.world.height - 200);
- //   game.add.existing(player.arrowLeft);
- //   player.arrowRight = new Arrow(game, 'keyR', 180, game.world.height - 200);
- //   game.add.existing(player.arrowRight);
+	player.facingRight = false;
+	player.scale.x *= -1;
 
 	//create the ground
 	var ground = platforms.create(0, 415, 'platform_med');
-	ground.scale.setTo(6.66, 25/17);		//scale the ground to fit the game (sprite is 300x68, & we need to to be 2000x16)
+	// ground.scale.setTo(6.66, 25/17);		//scale the ground to fit the game (sprite is 300x68, & we need to to be 2000x16)
 	ground.body.immovable = true;	//make the ground immovable so it won't fall when player touches it
+	ground.body.setSize(1000, 100, 0, 0);
+	ground.visible = false;
 
-	//make the left wall
-	var ledge = platforms.create(-17, 0, 'wall_big');
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(34, 600, 17, 0);	//adjust bounding box according to specifications
-	ledge = platforms.create(-17, 600, 'wall_big');
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(34, 600, 17, 0);	//adjust bounding box according to specifications
-	//make the right wall
-	ledge = platforms.create(1950, 0, 'wall_big');
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(34, 600, 17, 0);	//adjust bounding box according to specifications
-	ledge = platforms.create(1950, 600, 'wall_big');
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(34, 600, 17, 0);	//adjust bounding box according to specifications
+	//make the house
+	var house = platforms.create(0, 0, 'wall_big');
+	house.body.immovable = true;		//make wall immovable
+	house.body.setSize(102, 415, 0, 0);	//adjust bounding box according to specifications
+	house.visible = false;
 	
-	//make the platforms for platforming
-	ledge = platforms.create(0, 900, 'platform_med');		//platform A1
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(300, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(300, 900, 'platform_small');		//platform A2
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(-50, 650, 'platform_med');		//platform B
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(300, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(467, 650, 'platform_small');	//platform C
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(0, 400, 'platform_med');	//platform D
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(300, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(300, 400, 'platform_small');	//platform E
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(467, 200, 'platform_small');		//platform F
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(650, 200, 'platform_big');	//platform G
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(600, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(1250, 200, 'platform_med');	//platform H
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(300, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(1550, 200, 'platform_small');	//platform I
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(1700, 450, 'platform_med');	//platform J
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(300, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(1400, 650, 'platform_big');	//platform K
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(600, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(800, 650, 'platform_big');	//platform L
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(600, 34, 0, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(650, 900, 'platform_small');	//platform M
-	ledge.body.immovable = true;		//make wall immovable
-	ledge.body.setSize(150, 34, 0, 17);	//adjust bounding box according to specifications
-
-	//make the inner wall
-	ledge = platforms.create(600, 1100, 'wall_small');	//wall A
-	ledge.body.immovable = true;			//make wall immovable
-	ledge.body.setSize(34, 150, 17, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(600, 800, 'wall_med');	//wall B
-	ledge.body.immovable = true;			//make wall immovable
-	ledge.body.setSize(34, 300, 17, 17);	//adjust bounding box according to specifications
-	ledge = platforms.create(600, 200, 'wall_big');	//wall C
-	ledge.body.immovable = true;			//make wall immovable
-	ledge.body.setSize(34, 600, 17, 17);	//adjust bounding box according to specifications
-
-	// //spawn enemy pots
- //    var enemy = new Enemy(game, 'pot', 50, 550, 0, player, platforms);	//enemy 1
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
- //    enemy = new Enemy(game, 'pot', 250, 300, 0, player, platforms);	//enemy 2
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
- //    enemy = new Enemy(game, 'pot', 150, 300, 0, player, platforms);	//enemy 3
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
- //    enemy = new Enemy(game, 'pot', 50, 300, 0, player, platforms);	//enemy 4
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
-	// enemy = new Enemy(game, 'pot', 700, 100, 0, player, platforms);	//enemy 5
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
-	// enemy = new Enemy(game, 'pot', 1150, 550, 0, player, platforms);	//enemy 6
- //    game.add.existing(enemy);
- //    enemies.add(enemy);
-
-
- //    //spawn some ore pots
-	// var pot = new OrePot(game, 'pot', 150, 800, orePots, ores);	//pot A
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1900, 550, orePots, ores);	//pot B
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1800, 550, orePots, ores);	//pot C
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1700, 550, orePots, ores);	//pot D
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1350, 550, orePots, ores);	//pot E
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1250, 550, orePots, ores);	//pot F
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	// pot = new OrePot(game, 'pot', 1150, 550, orePots, ores);	//pot G
-	// game.add.existing(pot);
-	// orePots.add(pot);
-	
-	// // //spawn the level exit door thing
-	// var door = exit.create(1812, 1001, 'doorAtlas', 'Mineshaft1');
-	// door.animations.add('open', ['Mineshaft2','Mineshaft3'], 5, false);
-	// door.openSound = game.add.audio('smash');	//temporary sound until we get the door open noise
-	// door.body.immovable = true;
+	//spawn the level exit door thing
+	var door = exit.create(962, 245, 'doorAtlas', 'Mineshaft1');
+	door.animations.add('open', ['Mineshaft2','Mineshaft3'], 5, false);
+	door.openSound = game.add.audio('smash');	//temporary sound until we get the door open noise
+	door.body.immovable = true;
 
 	// //create special entities
 
@@ -173,7 +60,10 @@ var loadLevel_0 = function(game, player, platforms, enemies, orePots, exit, ores
 	// enemyContainer.platforms = platforms;	//enemyContainer will need access to platforms in order to spawn the wall
 
 	//spawn blacksmiths
-	var blacksmith_1 = specialEntities.create(500,1050, 'player');	//blacksmith @ start of level
+	var blacksmith_1 = specialEntities.create(403,230, 'smithyAtlas', 'SmithyMouthClosed');	//blacksmith @ start of level
+	blacksmith_1.animations.add('speak', ['SmithyMouthOpen','SmithyMouthClosed'], 5, true);
+	blacksmith_1.scale.x = -1/3;
+	blacksmith_1.scale.y = 1/3;
 	blacksmith_1.body.immovable = true;
 	blacksmith_1.specialFunction = speak;	//temporary thing until I set up a dialogue system
 	blacksmith_1.game = game;
@@ -183,19 +73,24 @@ var loadLevel_0 = function(game, player, platforms, enemies, orePots, exit, ores
 	blacksmith_1.typing = false;	//start off not typing
 	blacksmith_1.dialogueState = 0;	//start off in "not talked to"
 	blacksmith_1.player = player;
-	blacksmith_1.interacttext = new Instruction(game, 'keyE', 550, 1000, player);
+	blacksmith_1.interacttext = new Instruction(game, 'keyE', 320, 180, player);
+	blacksmith_1.speak_1 = game.add.audio('oldMan_1');
+	blacksmith_1.speak_2 = game.add.audio('oldMan_2');
+	blacksmith_1.speak_3 = game.add.audio('oldMan_3');
+	blacksmith_1.speak_4 = game.add.audio('oldMan_4');
+	blacksmith_1.sounds = [blacksmith_1.speak_1, blacksmith_1.speak_2, blacksmith_1.speak_3, blacksmith_1.speak_4];
 	game.add.existing(blacksmith_1.interacttext);
-	var blacksmith_2 = specialEntities.create(1900,317, 'player');	//blacksmith between end of sword course & start of hammer course
-	blacksmith_2.body.immovable = true;
-	blacksmith_2.specialFunction = speak;	//temporary thing until I set up a dialogue system
-	blacksmith_2.game = game;
-	blacksmith_2.dialogue = dialogue;
-	blacksmith_2.dialogueConvo = 1;	//1 corresponds to the hammer conversation in the tutorial dialogue JSON
-	blacksmith_2.dialogueLine = 0;	//start at line 0
-	blacksmith_2.dialogueState = 0;	//start off in "not talked to"
-	blacksmith_2.player = player;
-	blacksmith_2.interacttext = new Instruction(game, 'keyE', 1950, 267, player);
-	game.add.existing(blacksmith_2.interacttext);
+	// var blacksmith_2 = specialEntities.create(1900,317, 'player');	//blacksmith between end of sword course & start of hammer course
+	// blacksmith_2.body.immovable = true;
+	// blacksmith_2.specialFunction = speak;	//temporary thing until I set up a dialogue system
+	// blacksmith_2.game = game;
+	// blacksmith_2.dialogue = dialogue;
+	// blacksmith_2.dialogueConvo = 1;	//1 corresponds to the hammer conversation in the tutorial dialogue JSON
+	// blacksmith_2.dialogueLine = 0;	//start at line 0
+	// blacksmith_2.dialogueState = 0;	//start off in "not talked to"
+	// blacksmith_2.player = player;
+	// blacksmith_2.interacttext = new Instruction(game, 'keyE', 1950, 267, player);
+	// game.add.existing(blacksmith_2.interacttext);
 }
 
 var containEnemies = function() {
@@ -229,22 +124,25 @@ var speak = function() {
 	console.log(this.dialogueState);
 
 	if(this.dialogueState == 0) {	//if we haven't been talked to yet, perform some first-time setup
-	//create the dialogue box
-		this.dialogueBox = this.game.add.sprite(100, 400, 'dialogueBox');	//the dialogue background
+		//create the dialogue box
+		this.dialogueBox = this.game.add.sprite(170, 420, 'dialogueBox');	//the dialogue background
 		this.dialogueBox.fixedToCamera = true;
-		this.dialogueText = this.game.add.text(150, 445, '', {fontSize: '24px', fill: '#fff' });	//the dialogue text
+		this.dialogueText = this.game.add.text(220, 445, '', {fontSize: '24px', fill: '#fff' });	//the dialogue text
 		this.dialogueText.fixedToCamera = true;
-		this.nextText = this.game.add.text(875, 574, '', {fontSize: '24px', fill: '#aaa' });	//tells us what key to press to advance the dialogue
+		this.nextText = this.game.add.text(805, 594, '', {fontSize: '24px', fill: '#aaa' });	//tells us what key to press to advance the dialogue
 		this.nextText.fixedToCamera = true;
 		this.dialogueState = 1;	//switch to state 1 ("talking to player")
 		this.player.inDialogue = true;	//disable player movement & sword swinging
 		this.interacttext.kill();
 
 		//add the speaker's portrait
-		this.speakerImage = this.game.add.sprite(275, 285, 'player');	//replace with blacksmith sprite later
+		//this.speakerImage = this.game.add.sprite(275, 285, 'player');	//replace with blacksmith sprite later
+		this.speakerImage = this.game.add.sprite(275,285, 'smithyAtlas', 'SmithyMouthClosed');	//blacksmith @ start of level
+		this.speakerImage.animations.add('speak', ['SmithyMouthOpen','SmithyMouthClosed'], 5, true);
 		this.speakerImage.fixedToCamera = true;
 		this.speakerImage.anchor.set(0.5);
-		this.speakerImage.scale.setTo(1.5);
+		this.speakerImage.scale.x = -1.25/3;//setTo(1.5);
+		this.speakerImage.scale.y = 1.25/3;
 
 		//add the listener's portrait
 		this.listenerImage = this.game.add.sprite(725, 285, 'player');
@@ -295,6 +193,9 @@ var speak = function() {
 		this.dialogueState = 2;	//switch to state 2 ("done talking")
 	}
 	else {
+		this.speakerImage.animations.play('speak');
+		this.currentSound = this.sounds[this.game.rnd.integerInRange(0, 3)]
+		this.currentSound.play();
 		//update emote
 		if(this.dialogue[this.dialogueConvo][this.dialogueLine]['newEmote']) {	//if a new emote was specified
 			var newEmote = this.dialogue[this.dialogueConvo][this.dialogueLine]['newEmote'];
@@ -324,13 +225,16 @@ var speak = function() {
 			// show prompt for more text
 			this.nextText.text = '[E]';
 			this.nextText.anchor.setTo(1, 1);
+			//halt speaking animation & sound
+			this.speakerImage.animations.stop('speak');
+			this.currentSound.stop();
 			// un-lock input
 			this.typing = false;
 		}, this);
 			
 		// set bounds on dialogue
 		this.dialogueText.wordWrap = true;
-		this.dialogueText.wordWrapWidth = 715;
+		this.dialogueText.wordWrapWidth = 575;
 
 		// increment dialogue line
 		this.dialogueLine++;
